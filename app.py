63a59f3460f19edf9af2f4bbf869aa20cbe50cdd
Dimitri Gronoff <dimitri.gronoff@viacesi.fr>
2021-03-25 12:11:59 +0100

    branchement api ok

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
+from api import api  
 
 app = Flask(__name__)
 
 @app.route("/")
 def our_super_app():
+    resultat=api.OpenFoodFactApi()
+    products=resultat.getAll()
+
     # Flask utilise Jinja2 pour le templating
-    return render_template("/flask.html")
+    return render_template("/flask.html",arg=products)
                         
